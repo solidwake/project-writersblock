1ba63e9051f4f3ccf39e60751bd7ea6fd8312432
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-05 16:08:05 -0400

    add materialize and fontawesome cdn

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 // @desc    Login/landing page
 // @route   GET /
 router.get('/', (req, res) => {
-    res.send('Login')
+    res.render('Login')
 });
 
 // @desc    Dashboard
 // @route   GET /dashboard
 router.get('/dashboard', (req, res) => {
-    res.send('Dashboard')
+    res.render('Dashboard')
 });
 
 module.exports = router;
