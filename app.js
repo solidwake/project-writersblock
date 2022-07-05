37433bfd09d206387c005358b9e5868147a04584
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-05 15:59:03 -0400

    add routes

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+// Routes
+app.use('/', require('./routes/index'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
