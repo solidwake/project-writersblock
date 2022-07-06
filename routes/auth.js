ab1b63df898d199dbd998cb8785e3a6fe6e2604a
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-06 13:01:22 -0400

    add navbar and logout

--- routes/auth.js
+++ routes/auth.js
@@ -13,4 +13,15 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     res.redirect('/dashboard')
 });
 
+// @desc    Logout user
+// @route   /auth/logout
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if(err) {
+            return next(err)
+        }
+        res.redirect('/')
+    })
+})
+
 module.exports = router;
