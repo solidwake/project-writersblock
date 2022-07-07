c30ffa5331e93767d719a24eeb9c50f69f9fd604
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-06 23:47:30 -0400

    add sessions stored in datatbase

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
-const session = require('express-session')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const connectDB = require('./config/db');
 
 // Load config
@@ -28,11 +30,14 @@ app.set('view engine', '.hbs');
 app.set('views', './views');
 
 // Sessions
-app.use(session({
-    secret: 'write stuff',
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(
+    session({
+        secret: 'write stuff',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
+    })
+);
 
 // Passport middleware
 app.use(passport.initialize());
