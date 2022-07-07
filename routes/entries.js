bae27f743d00f9e47b3009e44f307d7935c58a10
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-07 14:31:48 -0400

    add user can make entry and store to database

--- routes/entries.js
+++ routes/entries.js
@@ -4,10 +4,23 @@ const { ensureAuth } = require('../middleware/auth');
 
 const Entry = require('../models/Entry')
 
-// @desc    Show Add page
+// @desc    Show Add Entry page
 // @route   GET /entries/add
 router.get('/add', ensureAuth, (req, res) => {
     res.render('entries/add')
+});
+
+// @desc    Process Add Entry form
+// @route   POST /entries
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Entry.create(req.body)
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        res.render('errors/500')
+    }
 });
 
 module.exports = router;
