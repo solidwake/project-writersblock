4e2ff40f46ff1d49cd981bc631faf801f85e0b7f
Idris Westbrook <westbrook.idris@gmail.com>
2022-07-09 16:49:28 -0400

    add delete entry from dashboard

--- routes/entries.js
+++ routes/entries.js
@@ -44,41 +44,63 @@ router.get('/', ensureAuth, async (req, res) => {
 // @desc    Show Edit Entry page
 // @route   GET /entries/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-    const entry = await Entry.findOne({
-        _id: req.params.id
-    }).lean()
-
-    if(!entry) {
-        return res.render('errors/404')
-    }
-
-    if(entry.user != req.user.id) {
-        res.redirect('/entries')
-    } else {
-        res.render('entries/edit', {
-            entry,
-        })
+    try {
+        const entry = await Entry.findOne({
+            _id: req.params.id
+        }).lean()
+    
+        if(!entry) {
+            return res.render('errors/404')
+        }
+    
+        if(entry.user != req.user.id) {
+            res.redirect('/entries')
+        } else {
+            res.render('entries/edit', {
+                entry,
+            })
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('errors/500')
     }
 });
 
 // @desc    Update Entry
 // @route   PUT /entries/:id
 router.put('/:id', ensureAuth, async (req, res) => {
-    let entry = await Entry.findById(req.params.id).lean()
+    try {
+        let entry = await Entry.findById(req.params.id).lean()
 
-    if(!entry) {
-        return res.render('errors/404')
+        if(!entry) {
+            return res.render('errors/404')
+        }
+        
+        if(entry.user != req.user.id) {
+            res.redirect('/entries')
+        } else {
+            entry = await Entry.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true,
+            })
+
+            res.redirect('/dashboard')
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('errors/500')
     }
-    
-    if(entry.user != req.user.id) {
-        res.redirect('/entries')
-    } else {
-        entry = await Entry.findOneAndUpdate({ _id: req.params.id }, req.body, {
-            new: true,
-            runValidators: true,
-        })
+});
 
+// @desc    Delete entry
+// @route   DELETE /entries/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Entry.remove({ _id: req.params.id })
         res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('errors/500')
     }
 });
 
